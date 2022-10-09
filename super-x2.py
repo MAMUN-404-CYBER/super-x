1d495935b90fb0d09fc4190da2f3d8ec24bf2fcc
Mamun Islam <112590492+Mamun026@users.noreply.github.com>
2022-10-10 03:01:16 +0600

    Update super-x2.py

--- super-x2.py
+++ super-x2.py
@@ -198,7 +198,7 @@ def crack2(idf,pwv):
 	bi = random.choice([u,k,kk,b,h,hh])
 	pers = loop*100/len(id2)
 	fff = '%'
-	print('\r%s [MAMUN] %s/%s  O%s/%s  %s%s%s'%(bi,loop,len(id2),ok,cp,int(pers),str(fff),x), end=' ');sys.stdout.flush()
+	print('\r%s [MAMUN] %s/%s  %s/%s  %s%s%s'%(bi,loop,len(id2),ok,cp,int(pers),str(fff),x), end=' ');sys.stdout.flush()
 	ua = random.choice(ugen).replace('\n','')
 	ses = requests.Session()
 	for pw in pwv:
